5591021c259f30d8e131bb648a5dcd4a30b56e10
kenkairon <ingeinfo2006@gmail.com>
2026-07-24 21:53:01 -0400

    configuracion para subir gitpage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/PortafolioIOS/" : "",
+  assetPrefix: isProd ? "/PortafolioIOS/" : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
